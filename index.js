edd4d73ccf310a96d769701517771e3a628fde6f
LimitToInfinity <21stLimitless@gmail.com>
2021-07-22 16:46:23 -0600

    First commit.

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const port = 9001;
+const cors = require('cors');
+const knex = require('knex');
+const databaseConfiguration = require('./knexfile').development;
+const database = knex(databaseConfiguration);
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/capstones', index);
+app.get('/capstones/:id', show);
+app.post('/capstones', create);
+
+function index(_, response) {
+  database('capstones')
+    .then(capstones => response.status(200).send(capstones));
+}
+
+function show(request, response) {
+  database('capstones')
+    .where('id', request.params.id)
+    .then(capstones => response.status(200).send(capstones[0]));
+}
+
+function create(request, response) {
+  const { capstone } = request.body;
+
+  database('capstones')
+    .insert(capstone)
+    .returning('*')
+    .then(capstones => response.status(201).send(capstones[0]));
+}
+
+app.listen(port, () => console.log(`listening on port ${port}`));
